5abcf2dae850262e34cde1ab65b5635ae70f9d06
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-03-27 17:31:16 +0700

    finalproject

---
+++ script.js
@@ -0,0 +1,25 @@
+function nasiGoreng() {
+    let nasi = document.getElementById("nasi");
+    let total = document.getElementById("total");
+    total.value = nasi.value * 12000;
+}
+function mieAyam() {
+    let mie = document.getElementById("mie");
+    let total1 = document.getElementById("total1");
+    total1.value = mie.value * 10000;
+}
+function lemonTea() {
+    let lemon = document.getElementById("lemon");
+    let total2 = document.getElementById("total2");
+    total2.value = lemon.value * 6000;
+}
+function cappuccino() {
+    let cap = document.getElementById("cap");
+    let total3 = document.getElementById("total3");
+    total3.value = cap.value * 8000;
+}
+function pesanan() {
+    let cap = document.getElementById("cap");
+    let total3 = document.getElementById("total3");
+    total3.value = cap.value * 8000;
+}
